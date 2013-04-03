545d58bfeb97cfaae8e5f0d2262b0a0034395b47
sea_mountain <seaforestmountain@gmail.com>
2013-04-04 02:00:20 +0900

    Can show transparent texture, but rendering is wrong.
    I cannot show the texture indices probably.

--- triangle.jsx
+++ triangle.jsx
@@ -45,7 +45,7 @@ class _Main {
       gl.clear(gl.COLOR_BUFFER_BIT);
 
       gl.enable(gl.DEPTH_TEST);
-			//gl.enable(gl.CULL_FACE);
+      //gl.enable(gl.CULL_FACE);
       gl.enable(gl.BLEND);
       //gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
       gl.blendFuncSeparate(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA, gl.ONE, gl.ONE);
@@ -54,102 +54,80 @@ class _Main {
       gl.uniform1f(slow, (frameNumber % 100) / 100);
 
       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-             0.0,   0.0,  0.0,  1.0,
-             0.0,   0.3,  0.0,  1.0,
-             0.3,   0.0,  0.0,  1.0,
-             0.3,   0.3,  0.0,  1.0,
+            0.3,   0.3,  0.0,  1.0,
+            0.0,   0.3,  0.0,  1.0,
+            0.3,   0.0,  0.0,  1.0,
+            0.0,   0.0,  0.0,  1.0,
 
-             0.7,   0.7,  0.0,  1.0,
-             0.7,   0.9,  0.0,  1.0,
-             0.9,   0.7,  0.0,  1.0,
-             0.9,   0.9,  0.0,  1.0,
+            0.7,   0.7,  0.0,  1.0,
+            0.7,   0.9,  0.0,  1.0,
+            0.9,   0.7,  0.0,  1.0,
+            0.9,   0.9,  0.0,  1.0,
 
             -0.3,  -0.3,  0.0,  1.0,
             -0.3,  -0.5,  0.0,  1.0,
             -0.5,  -0.3,  0.0,  1.0,
             -0.5,  -0.5,  0.0,  1.0
-      ]), gl.STATIC_DRAW);
+            ]), gl.STATIC_DRAW);
 
       gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
       gl.drawArrays(gl.TRIANGLE_STRIP, 4, 4);
       gl.drawArrays(gl.TRIANGLE_STRIP, 8, 4);
-      //gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
-      //gl.drawElements(gl.TRIANGLES, 8, gl.UNSIGNED_SHORT, 0);
-      //gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
 
       dom.window.setTimeout(drawFrame, 100);
     }
     drawFrame();
 
-    //prog.vertexPosAttrib = gl.getAttribLocation(prog, 'aVertexPosition');
-    //prog.samplerUniform = gl.getUniformLocation(prog, 'uSampler');
-    //gl.enableVertexAttribArray(prog.vertexPosArray);
-    //gl.vertexAttribPointer(prog.vertexPosAttrib, vertexPosBuffer.itemSize, gl.FLOAT, false, 0, 0);
     var samplerUniform = gl.getUniformLocation(prog, 'uSampler');
     var vertexPosAttrib = gl.getAttribLocation(prog, 'aVertexPosition');
+    var textureCoordAttribute = gl.getAttribLocation(prog, 'vTextureCoord');
     var texture = gl.createTexture();
-    //var img = new Image();
-		var img = dom.createElement("img") as HTMLImageElement;
+    var img = dom.createElement("img") as HTMLImageElement;
 
-    gl.vertexAttribPointer(vertexPosAttrib, 3, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(vertexPosAttrib, 4, gl.FLOAT, false, 0, 0);
+
     var textureCoordinates  = new Float32Array([
-        0.0, 0.0,
-        1.0, 0.0,
         1.0, 1.0,
         0.0, 1.0,
-
-        // Back face
         1.0, 0.0,
+        0.0, 0.0,
+
+        0.0, 0.0,
+        1.0, 0.0,
+        0.0, 1.0,
         1.0, 1.0,
-        0.0, 1.0,
-        0.0, 0.0,
 
-        // Top face
-        0.0, 1.0,
         0.0, 0.0,
         1.0, 0.0,
+        0.0, 1.0,
+        1.0, 1.0,
+
+        0.0, 0.0,
+        1.0, 0.0,
+        0.0, 1.0,
         1.0, 1.0
-          //0.0, 0.0,
-          //1.0, 0.0,
-          //1.0, 1.0,
-          //0.0, 1.0
+        ]);
+    gl.bufferData(gl.ARRAY_BUFFER, textureCoordinates, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(textureCoordAttribute, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(textureCoordAttribute);
 
-          //0.0, 0.0,
-          //0.0, 1.0,
-          //1.0, 0.0,
-          //1.0, 1.0,
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates), gl.STATIC_DRAW);
 
-          //0.0, 0.0,
-          //0.0, 1.0,
-          //1.0, 0.0,
-          //1.0, 1.0
-          ]);
-    //gl.vertexAttribPointer(vertexPosAttrib, vTexture, gl.FLOAT, false, 0, 0);
-    
-      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates), gl.STATIC_DRAW);
-
-		img.addEventListener("load", (e) -> {
+    img.addEventListener("load", (e) -> {
       gl.activeTexture(gl.TEXTURE0);
       gl.bindTexture(gl.TEXTURE_2D, texture);
       //gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-			gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // 画像の上下反転
+      gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // 画像の上下反転
       gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
       gl.uniform1i(samplerUniform, 0);
-      //gl.drawArrays(gl.TRIANGLE_STRIP, 0, 3);
-      //
 
       var size = 256;
-      //gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, size, size, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
-      //gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, size, size, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
-      //gl.texImage2D(gl.TEXTURE_2D, 0, gl.DEPTH_COMPONENT, size, size, 0, gl.DEPTH_COMPONENT, gl.UNSIGNED_SHORT, null);
-      //gl.uniform1i(samplerUniform, 0);
     });
     //img.src = 'icon.jpg';
     img.src = 'icon.gif';
-
   }
 }
